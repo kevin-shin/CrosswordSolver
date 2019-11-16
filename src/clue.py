909a88867f90d60de1216907830239fe02dc87d3
avikboss <abosshar@macalester.edu>
2019-11-16 11:38:09 -0600

    add puzzle and clue classes

---
+++ src/clue.py
@@ -0,0 +1,10 @@
+class Clue:
+    def __init__(self,row,column,length,direction,description,number):
+        self.row = row
+        self.column = column
+        self.length = length
+        self.direction = direction
+        self.description = description
+        self.number = number
+    
+        
---
+++ src/puzzle.py
@@ -0,0 +1,3 @@
+class Puzzle():
+    def __init__(self,size):
+        self.size = size
